eaa63635e047245be8a957fc0386149e420dfc7e
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-14 10:54:26 +0100

    Add linting to CI and style code (#19)
    * Add `{lintr}` config
    
    * `styler::style_pkg()`; resolve lintr warnings
    
    * Add linting to CI
    
    * Add PR commands GHA
    
    Adds `/document` and `/style` as PR commands
    
    * Add `lintr` as dev dependency
    
    * Resolve `lintr` warnings
    
    * Add `.lintr` to buildignore
    
    * Roxygenise
    
    * Switch back to `camelCase` for `ShinyApp` args
    
    * Allow `camelCase` object names
    
    Often used in Shiny arguments
    
    * Use `setup-renv` instead of `setup-r-dependencies`
    
    See https://github.com/r-lib/actions/issues/911

--- R/mod_daterange.R
+++ R/mod_daterange.R
@@ -19,10 +19,10 @@ mod_date_range_ui <- function(id) {
       format = "yyyy-mm",
     ),
     sliderInput(ns("slider"), "Date range:",
-                min = as.Date("2019-04-01", "%Y-%m-%d"),
-                max = as.Date("2024-08-01", "%Y-%m-%d"),
-                value = c(as.Date("2019-04-01"), as.Date("2024-08-01")),
-                timeFormat = "%Y-%m"
+      min = as.Date("2019-04-01", "%Y-%m-%d"),
+      max = as.Date("2024-08-01", "%Y-%m-%d"),
+      value = c(as.Date("2019-04-01"), as.Date("2024-08-01")),
+      timeFormat = "%Y-%m"
     )
   )
 }
@@ -32,7 +32,5 @@ mod_date_range_ui <- function(id) {
 #' @noRd
 mod_date_range_server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    ns <- session$ns
   })
 }
-
