d8e82c52325d3c4bbc0f0e8245e24f75e83173d3
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-16 19:16:27 +0100

    Slight refactor of `monthly_count_plot()`

--- R/fct_monthly_count_plot.R
+++ R/fct_monthly_count_plot.R
@@ -1,15 +1,16 @@
 #' monthly_count_plot
 #'
-#' @description A fct function
+#' Generates a bar plot of the number of records per month for a given concept.
 #'
-#' @return The return value, if any, from executing the function.
+#' @return A ggplot2 object containing the bar plot, or `NULL` if no data is provided.
 #'
 #' @importFrom ggplot2 ggplot aes geom_bar ggtitle xlab ylab
-#'
 #' @noRd
-monthly_count_plot <- function(monthly_counts, name) {
+monthly_count_plot <- function(monthly_counts, plot_title) {
+  if (is.null(monthly_counts)) return(NULL)
+
   stopifnot(is.data.frame(monthly_counts))
-  stopifnot(is.character(name))
+  stopifnot(is.character(plot_title))
   stopifnot(all(c("date_year", "date_month", "person_count") %in% colnames(monthly_counts)))
 
   monthly_counts$date <- .convert_to_date(monthly_counts$date_year, monthly_counts$date_month)
@@ -17,7 +18,7 @@ monthly_count_plot <- function(monthly_counts, name) {
   date <- person_count <- NULL
   ggplot(monthly_counts, aes(x = date, y = person_count)) +
     geom_bar(stat = "identity") +
-    ggtitle(name) +
+    ggtitle(plot_title) +
     xlab("Month") +
     ylab("Number of records")
 }
