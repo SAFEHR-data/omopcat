490caff882590a284b27561c9a9e28d9548f6350
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-23 15:20:53 +0100

    Explicitly specify column types for dummy data to avoid errors later on

--- dev/test_db/insert_dummy_tables.R
+++ dev/test_db/insert_dummy_tables.R
@@ -4,6 +4,8 @@
 
 cli::cli_h1("Inserting dummy tables")
 
+library(readr)
+
 dir <- Sys.getenv("EUNOMIA_DATA_FOLDER")
 name <- Sys.getenv("TEST_DB_NAME")
 version <- Sys.getenv("TEST_DB_OMOP_VERSION")
@@ -31,18 +33,40 @@ write_table <- function(data, con, table) {
 }
 
 ## Load dummy data and write tables to database
-dummy_measurements <- read.csv(here::here("dev/test_db/dummy/measurement.csv"))
+## We explicitly set the column types for columns that are needed later down the pipeline
+dummy_measurements <- read_csv(
+  here::here("dev/test_db/dummy/measurement.csv"),
+  col_types = cols(
+    measurement_id = col_double(),
+    person_id = col_double(),
+    measurement_concept_id = col_double(),
+    measurement_date = col_date(),
+    value_as_number = col_double(),
+    value_as_concept_id = col_double(),
+  )
+)
 write_table(dummy_measurements, con, "measurement")
 
-dummy_observations <- read.csv(here::here("dev/test_db/dummy/observation.csv"))
+dummy_observations <- read_csv(here::here(
+  "dev/test_db/dummy/observation.csv"),
+  col_types = cols(
+      observation_id = col_double(),
+      person_id = col_double(),
+      observation_concept_id = col_double(),
+      observation_date = col_date(),
+      value_as_number = col_double(),
+      value_as_string = col_logical(),
+      value_as_concept_id = col_double(),
+    )
+)
 write_table(dummy_observations, con, "observation")
 
 # Sanity check: read the data back and make sure its consistent
 db_measurements <- DBI::dbReadTable(con, "measurement")
-stopifnot(all.equal(db_measurements, dummy_measurements))
+stopifnot(all.equal(db_measurements, as.data.frame(dummy_measurements)))
 
 db_observations <- DBI::dbReadTable(con, "observation")
-stopifnot(all.equal(db_observations, dummy_observations))
+stopifnot(all.equal(db_observations, as.data.frame(dummy_observations)))
 
 # Load the CMD object to verify integrity of the schema after insertions
 cdm <- CDMConnector::cdm_from_con(
