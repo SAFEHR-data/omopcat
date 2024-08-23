92b91e494c5a250bab1c9dc514d176e3c3affa5a
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-23 16:05:43 +0100

    Reduce test data size

--- dev/test_db/produce_test_data.R
+++ dev/test_db/produce_test_data.R
@@ -1,6 +1,8 @@
 cli::cli_h1("Producing test data")
 
-library(dplyr)
+suppressPackageStartupMessages({
+  library(dplyr)
+})
 
 dir <- Sys.getenv("EUNOMIA_DATA_FOLDER")
 name <- Sys.getenv("TEST_DB_NAME")
@@ -13,21 +15,37 @@ con <- DBI::dbConnect(
 withr::defer(DBI::dbDisconnect(con))
 
 # Function to write results from a table to the test data folder
-write_results <- function(con, table) {
+read_table <- function(con, table) {
   schema <- Sys.getenv("TEST_DB_RESULTS_SCHEMA")
   # Get all rows from the table
   query <- glue::glue("SELECT * FROM {schema}.{table}")
   # Run the query and write results
-  path <- here::here(glue::glue("inst/test_data/{table}.csv"))
-  cli::cli_alert_info("Writing {table} to {path}")
   con |>
-    DBI::dbGetQuery(query) |>
-    arrange(across(everything())) |>
-    readr::write_csv(file = path)
+    DBI::dbGetQuery(query)
 }
 
-# Write all results to the test data folder
+# Get the relevant tables and filter
 table_names <- c("calypso_concepts", "calypso_monthly_counts", "calypso_summary_stats")
-purrr::walk(table_names, write_results, con = con)
+tables <- purrr::map(table_names, read_table, con = con)
+names(tables) <- table_names
+
+# Keep only concepts for which we have summary statistics
+keep_concepts <- tables$calypso_summary_stats$concept_id
+tables <- purrr::map(tables, ~ .x[.x$concept_id %in% keep_concepts, ])
+
+# Keep only data from 2019 onwards
+monthly_counts <- tables$calypso_monthly_counts
+filtered_monthly <- monthly_counts[monthly_counts$date_year >= 2019, ]
+tables$calypso_monthly_counts <- filtered_monthly
+
+# Filter the other tables to match the concepts left over after year filtering
+tables <- purrr::map(tables, ~ .x[.x$concept_id %in% filtered_monthly$concept_id, ])
+
+# Write all results to the test data folder
+purrr::iwalk(tables, function(tbl, name) {
+  path <- here::here(glue::glue("inst/test_data/{name}.csv"))
+  cli::cli_alert_info("Writing {name} to {path}")
+  readr::write_csv(tbl, file = path)
+})
 
 cli::cli_alert_success("Test data produced")
