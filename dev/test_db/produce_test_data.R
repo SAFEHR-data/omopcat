dc6dd43f2b87bff97067f6067a96f2926bdf6cac
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-23 15:32:29 +0100

    Just do a simple `arrange()` across all columns to order data

--- dev/test_db/produce_test_data.R
+++ dev/test_db/produce_test_data.R
@@ -15,16 +15,17 @@ con <- DBI::dbConnect(
 withr::defer(DBI::dbDisconnect(con))
 
 # Function to write results from a table to the test data folder
-write_results <- function(con, table, order_by = "") {
+write_results <- function(con, table) {
   schema <- Sys.getenv("TEST_DB_RESULTS_SCHEMA")
   # Get all rows from the table
-  query <- glue::glue("SELECT * FROM {schema}.{table} {order_by};")
+  query <- glue::glue("SELECT * FROM {schema}.{table}")
   # Run the query and write results
   path <- here::here(glue::glue("inst/test_data/{table}.csv"))
   cli::cli_alert_info("Writing {table} to {path}")
   con |>
     DBI::dbGetQuery(query) |>
-    write_csv(file = path)
+    arrange(across(everything())) |>
+    readr::write_csv(file = path)
 }
 
 # Write all results to the test data folder
