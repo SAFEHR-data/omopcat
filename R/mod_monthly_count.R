9f0f9533f596f5570f5100f27ad83ef080bef7ff
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-27 14:26:40 +0100

    Add reactivity for daterange filtering (#37)
    * Test daterange selection
    
    * Apply daterange filtering before plotting monthly counts
    
    * Add dependencies
    
    * Fix assertions; allow for empty `selected_concept_id` but return empty plot
    
    * Test that monthly count plot reacts to daterange filtering
    
    * Fix formatting

--- R/mod_monthly_count.R
+++ R/mod_monthly_count.R
@@ -23,25 +23,45 @@ mod_monthly_count_ui <- function(id) {
 #' @param selected_concept Reactive value containing the selected concept, used for filtering
 #'
 #' @noRd
-mod_monthly_count_server <- function(id, data, selected_concept) {
+mod_monthly_count_server <- function(id, data, selected_concept, selected_dates) {
   stopifnot(is.data.frame(data))
   stopifnot(is.reactive(selected_concept))
+  stopifnot(is.reactive(selected_dates))
 
 
   moduleServer(id, function(input, output, session) {
-    ## Filter data based on selected_row
+    ## Filter data based on selected concept and datea range
     selected_concept_id <- reactive(selected_concept()$concept_id)
     selected_concept_name <- reactive(selected_concept()$concept_name)
     filtered_monthly_counts <- reactive({
-      if (!length(selected_concept_id())) {
+      if (is.null(selected_concept_id())) {
         return(NULL)
       }
-      data[data$concept_id == selected_concept_id(), ]
+      req(selected_dates()) # we expect always to have dates selected
+      out <- data[data$concept_id == selected_concept_id(), ]
+      .filter_dates(out, selected_dates())
     })
+
     output$monthly_count_plot <- renderPlot({
       ## Return empty plot if no data is selected
-      if (is.null(filtered_monthly_counts())) return(NULL)
+      if (is.null(filtered_monthly_counts()) || nrow(filtered_monthly_counts()) == 0) {
+        return(NULL)
+      }
       monthly_count_plot(filtered_monthly_counts(), selected_concept_name())
     })
   })
+}
+
+
+
+.filter_dates <- function(monthly_counts, date_range) {
+  range_years <- lubridate::year(date_range)
+  range_months <- lubridate::month(date_range)
+
+  date_year <- date_month <- NULL
+  dplyr::filter(
+    monthly_counts,
+    date_year >= range_years[1] & date_year <= range_years[2],
+    date_month >= range_months[1] & date_month <= range_months[2]
+  )
 }
