96374bc1b7c340a7d17002715cafaad7007af180
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-08-28 18:04:11 +0100

    Use deployment envvars in app

--- R/fct_connect_to_test_db.R
+++ R/fct_connect_to_test_db.R
@@ -11,9 +11,9 @@
 #'
 #' @noRd
 connect_to_test_db <- function() {
-  dir <- Sys.getenv("EUNOMIA_DATA_FOLDER")
-  name <- Sys.getenv("TEST_DB_NAME")
-  version <- Sys.getenv("TEST_DB_OMOP_VERSION")
+  dir <- Sys.getenv("CALYPSO_DATA_PATH")
+  name <- Sys.getenv("CALYPSO_DB_NAME")
+  version <- Sys.getenv("CALYPSO_DB_OMOP_VERSION")
   # Connect to the duckdb test database
   dbConnect(duckdb(dbdir = glue("{dir}/{name}_{version}_1.0.duckdb")))
 }
