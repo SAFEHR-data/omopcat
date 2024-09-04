872e36447c7c276c880c1343fadf3a786a0c0940
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-04 08:11:31 +0100

    Fix bug in app startup and refactor (#46)
    * Only check envvars when running in prod
    
    * Update tests: module servers should fail on missing inputs
    
    * Fix: get rid of warning/error at app startup when no concept is selected yet
    
    Use `shiny::req()` to simplify checking for existence of reactive inputs
    
    * Refactor: use single module for dashboard plots
    
    * Refactor: update UI elements for plots
    
    * Make plotting module generic for the type of plot
    
    Fixes #30
    
    * Move common module server tests to `test-mod_plots.R`
    
    * Rename test files, they now only only contain business-logic tests
    
    * Remove old modules
    
    * Update doc
    
    * Pass linter

--- R/app_server.R
+++ R/app_server.R
@@ -7,8 +7,6 @@
 app_server <- function(input, output, session) {
   # Get the input tables
   concepts_table <- get_concepts_table()
-  monthly_counts <- get_monthly_counts()
-  summary_stats <- get_summary_stats()
 
   selected_data <- mod_select_concepts_server("select_concepts", concepts_table)
   mod_date_range_server("date_range")
@@ -16,10 +14,8 @@ app_server <- function(input, output, session) {
   selected_row <- mod_datatable_server("totals", selected_data)
   selected_dates <- mod_date_range_server("date_range")
 
-  # TODO: refactor monthly_count and summary_stat modules into a single module?n
-  # https://github.com/SAFEHR-data/omop-data-catalogue/issues/30
-  mod_monthly_count_server("monthly_count", monthly_counts, selected_row, selected_dates)
-  mod_summary_stat_server("summary_stat", summary_stats, selected_row)
+  mod_plots_server("monthly_counts", selected_row, selected_dates, type = "monthly_counts")
+  mod_plots_server("summary_stats", selected_row, type = "summary_stats")
 
   mod_export_tab_server("export_tab", selected_data)
 }
---
+++ R/mod_plots.R
@@ -0,0 +1,82 @@
+#' plots UI Function
+#'
+#' Shiny module responsible for producing the summary plots
+#' UI elements for the dashboard
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_plots_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    plotOutput(ns("summary_plot"), height = 250)
+  )
+}
+
+#' plots Server function
+#'
+#' Generates the plot of the requested `type`. When no concept is selected, or no data is available,
+#' an error is raised.
+#'
+#' @param data `data.frame` containing the data to be plotted.
+#' @param selected_concept Reactive value containing the selected concept, used for filtering
+#' @param selected_dates Optional reactive value if date filtering needs to be applied
+#' @param type The type of plot to be generated.
+#'
+#' @noRd
+mod_plots_server <- function(id, selected_concept, selected_dates = NULL,
+                             type = c("monthly_counts", "summary_stats")) {
+  stopifnot(is.reactive(selected_concept))
+  stopifnot(is.reactive(selected_dates) || is.null(selected_dates))
+
+  plot_func <- switch(type,
+    monthly_counts = monthly_count_plot,
+    summary_stats = summary_stat_plot,
+    cli::cli_abort("Invalid type: {type}")
+  )
+  data <- switch(type,
+    monthly_counts = get_monthly_counts(),
+    summary_stats = get_summary_stats(),
+    cli::cli_abort("Invalid type: {type}")
+  )
+
+  moduleServer(id, function(input, output, session) {
+    selected_concept_id <- reactive(selected_concept()$concept_id)
+    selected_concept_name <- reactive(selected_concept()$concept_name)
+
+    ## Filter data based on selected concept and date range
+    filtered_data <- reactive({
+      req(length(selected_concept_name()) > 0)
+      out <- data[data$concept_id == selected_concept_id(), ]
+
+      if (!is.null(selected_dates)) {
+        req(selected_dates())
+        out <- .filter_dates(out, selected_dates())
+      }
+      out
+    })
+
+    output$summary_plot <- renderPlot({
+      ## Return empty plot if no data is available
+      req(filtered_data())
+      if (nrow(filtered_data()) == 0) {
+        stop("No data available for the selected date range")
+      }
+      plot_func(filtered_data(), selected_concept_name())
+    })
+  })
+}
+
+
+.filter_dates <- function(x, date_range) {
+  date_range <- as.Date(date_range)
+  if (date_range[2] < date_range[1]) {
+    stop("Invalid date range, end date is before start date")
+  }
+
+  dates <- lubridate::make_date(year = x$date_year, month = x$date_month)
+  keep_dates <- dplyr::between(dates, date_range[1], date_range[2])
+  dplyr::filter(x, keep_dates)
+}
