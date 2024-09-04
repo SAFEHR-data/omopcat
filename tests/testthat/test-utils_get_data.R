5fca83d449362713df7f1689d16162a763cbd98f
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-04 16:54:16 +0100

    No need for curly braces here

--- tests/testthat/test-utils_get_data.R
+++ tests/testthat/test-utils_get_data.R
@@ -65,36 +65,20 @@ test_that("Test data parquet files exist and are accessible", {
 #   1 < LOW_FREQUENCY_REPLACEMENT < 1000
 test_that("Test low frequency stats replacement for monthly counts", {
   mock_monthly_counts <- data.frame(
-    concept_id =         c(1, 2, 3, 4, 5, 6, 7),
-    person_count =       c(0, 0, 1000, 1000, 1, 1000, 1),
+    concept_id = c(1, 2, 3, 4, 5, 6, 7),
+    person_count = c(0, 0, 1000, 1000, 1, 1000, 1),
     records_per_person = c(0, 1000, 0, 1000, 1000, 1, 1)
   )
   replacement <- as.double(Sys.getenv("LOW_FREQUENCY_REPLACEMENT"))
   results <- .manage_low_frequency(mock_monthly_counts)
   expect_true(nrow(results) == 4)
-  expect_true(nrow({
-    dplyr::filter(results, person_count == 0)
-  }) == 0)
-  expect_true(nrow({
-    dplyr::filter(results, person_count == 1)
-  }) == 0)
-  expect_true(nrow({
-    dplyr::filter(results, person_count == replacement)
-  }) == 2)
-  expect_true(nrow({
-    dplyr::filter(results, person_count == 1000)
-  }) == 2)
-  expect_true(nrow({
-    dplyr::filter(results, records_per_person == 0)
-  }) == 0)
-  expect_true(nrow({
-    dplyr::filter(results, records_per_person == 1)
-  }) == 0)
-  expect_true(nrow({
-    dplyr::filter(results, records_per_person == replacement)
-  }) == 2)
-  expect_true(nrow({
-    dplyr::filter(results, records_per_person == 1000)
-  }) == 2)
+  expect_true(nrow(dplyr::filter(results, person_count == 0)) == 0)
+  expect_true(nrow(dplyr::filter(results, person_count == 1)) == 0)
+  expect_true(nrow(dplyr::filter(results, person_count == replacement)) == 2)
+  expect_true(nrow(dplyr::filter(results, person_count == 1000)) == 2)
+  expect_true(nrow(dplyr::filter(results, records_per_person == 0)) == 0)
+  expect_true(nrow(dplyr::filter(results, records_per_person == 1)) == 0)
+  expect_true(nrow(dplyr::filter(results, records_per_person == replacement)) == 2)
+  expect_true(nrow(dplyr::filter(results, records_per_person == 1000)) == 2)
 })
 
