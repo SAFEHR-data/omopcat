d1262f5d5bd31c1cda1ba03c2f2e10c87ad494f4
South <Andrew.South@UCLH201.NET>
2024-09-11 14:17:05 +0100

    move detection of selected dates earlier because needed to calc num records, and remove superfluous? line

--- R/app_server.R
+++ R/app_server.R
@@ -9,12 +9,14 @@ app_server <- function(input, output, session) {
   concepts_table <- get_concepts_table()
 
   selected_data <- mod_select_concepts_server("select_concepts", concepts_table)
-  mod_date_range_server("date_range")
-
-  #populates main concepts table
-  selected_row <- mod_datatable_server("totals", selected_data)
 
   selected_dates <- mod_date_range_server("date_range")
+
+
+
+  #populate main concepts table
+  selected_row <- mod_datatable_server("totals", selected_data)
+
 
   mod_plots_server("monthly_counts", selected_row, selected_dates, type = "monthly_counts")
   mod_plots_server("summary_stats", selected_row, type = "summary_stats")
