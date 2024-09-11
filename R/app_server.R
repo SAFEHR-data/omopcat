cadc4dfe25726ae2ec7ce5eb89293a4e76d5bd94
South <Andrew.South@UCLH201.NET>
2024-09-11 17:43:16 +0100

    started to add counts in date range but currently failing

--- R/app_server.R
+++ R/app_server.R
@@ -12,11 +12,27 @@ app_server <- function(input, output, session) {
 
   selected_dates <- mod_date_range_server("date_range")
 
+  #use selected dates to calc num patients and records per concept
+  ##join counts onto selected_data
+  ##start with mean records_per_person
+  ##later we need to add record_count & calc person_count=record_count/mean(records_per_person)
 
+  #cat(selected_dates)
+
+  monthly_counts <- get_monthly_counts()
+
+  #Hmmm? filter_dates is giving
+  #Error in as.Date.default: do not know how to convert 'date_range' to class “Date”
+  df_counts_in_dates <- filter_dates(monthly_counts, selected_dates) |>
+    group_by(concept_id) |>
+    summarise(records_per_person = mean(records_per_person))
+
+  df_concepts_sel_plus_counts <- selected_data #|>
+    #left_join(df_counts_in_dates, by = "concept_id")
 
   #populate main concepts table
-  selected_row <- mod_datatable_server("totals", selected_data)
-
+  #selected_row <- mod_datatable_server("totals", selected_data)
+  selected_row <- mod_datatable_server("totals", df_concepts_sel_plus_counts)
 
   mod_plots_server("monthly_counts", selected_row, selected_dates, type = "monthly_counts")
   mod_plots_server("summary_stats", selected_row, type = "summary_stats")
