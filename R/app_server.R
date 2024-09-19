70bcca380f66ebd314a5d880e4280d496365444b
South <Andrew.South@UCLH201.NET>
2024-09-19 11:38:38 +0100

    moved record count code into mod_datatable and now it doesn't work, records_per_person doesn't get addeded, doh! ??

--- R/app_server.R
+++ R/app_server.R
@@ -12,22 +12,8 @@ app_server <- function(input, output, session) {
 
   selected_dates <- mod_date_range_server("date_range")
 
-  #use selected dates to calc num patients and records per concept
-  ##join onto selected_data
-  ##start with mean records_per_person
-  ##later we need to add record_count & calc person_count=record_count/mean(records_per_person)
-
-  monthly_counts <- get_monthly_counts()
-
-  selected_data_plus_counts <- reactive({
-    filter_dates(monthly_counts, selected_dates()) |>
-    group_by(concept_id) |>
-    summarise(records_per_person = mean(records_per_person)) |>
-    dplyr::right_join(selected_data(), by = "concept_id")
-  })
-
   #populate main concepts table
-  selected_row <- mod_datatable_server("totals", selected_data_plus_counts)
+  selected_row <- mod_datatable_server("totals", selected_data, selected_dates)
 
   mod_plots_server("monthly_counts", selected_row, selected_dates, type = "monthly_counts")
   mod_plots_server("summary_stats", selected_row, type = "summary_stats")
--- R/mod_datatable.R
+++ R/mod_datatable.R
@@ -21,8 +21,9 @@ mod_datatable_ui <- function(id) {
 #' @return The selected row as a reactive object
 #'
 #' @noRd
-mod_datatable_server <- function(id, data) {
+mod_datatable_server <- function(id, data, selected_dates = NULL) {
   stopifnot(is.reactive(data))
+  stopifnot(is.reactive(selected_dates) || is.null(selected_dates))
 
   moduleServer(id, function(input, output, session) {
     output$datatable <- DT::renderDT(data(), selection = list(
@@ -31,6 +32,24 @@ mod_datatable_server <- function(id, data) {
       target = "row"
     ))
 
-    reactive(data()[input$datatable_rows_selected, ])
+    #use selected dates to calc num patients and records per concept
+    ##join onto selected_data
+    ##start with mean records_per_person
+    ##later we need to add record_count & calc person_count=record_count/mean(records_per_person)
+
+    #TODO FIND OUT WHY records_per_person NO LONGER ADDED TO TABLE
+    #when this code moved here from app_server.R ???
+
+    monthly_counts <- get_monthly_counts()
+
+    selected_data_plus_counts <- reactive({
+      filter_dates(monthly_counts, selected_dates()) |>
+        group_by(concept_id) |>
+        summarise(records_per_person = mean(records_per_person)) |>
+        #dplyr::right_join(selected_data(), by = "concept_id")
+        dplyr::right_join(data(), by = "concept_id")
+    })
+
+    reactive(selected_data_plus_counts()[input$datatable_rows_selected, ])
   })
 }
