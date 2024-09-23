aec541f6bfecfb3d94a4497d421bdf4db4a55320
South <Andrew.South@UCLH201.NET>
2024-09-23 09:55:35 +0100

    start making simpler test data explicitly to test counting

--- tests/testthat/test-mod_datatable.R
+++ tests/testthat/test-mod_datatable.R
@@ -40,3 +40,37 @@ test_that("module ui works", {
     expect_true(i %in% names(fmls))
   }
 })
+
+#to test calculation of record and patient counts
+#
+#start making simpler test data explicitly to test counting
+df_month_counts <- data.frame(
+  concept_id = rep(c(1,2,3), each = 3),
+  date_year = rep(c(2019,2020,2021), times = 3),
+  date_month = 1,
+  person_count = rep(c(10,20,30), each = 3),
+  records_per_person = 1
+)
+
+df_concepts <- data.frame(
+  concept_id = c(1,2,3),
+  concept_name = c(
+    "2019",
+    "2019-2020",
+    "2019-2021"
+  ),
+  domain_id = "Drug",
+  vocabulary_id = "LOINC",
+  concept_class_id = NA,
+  standard_concept = "S",
+  concept_code = NA
+)
+
+
+# mock_monthly_counts <- data.frame(
+#   concept_id = rep(c(1,2), each = 3),
+#   date_year = c(2019, 2020, 2021, 2019L, 2020L, 2020L, 2020L, 2019L, 2019L),
+#   date_month = 1,
+#   person_count = c(1, 1, 3, 4, 2, 3, 2, 4, 1),
+#   records_per_person = c(1, 1, 1, 1, 1, 1, 1, 1, 1)
+# )
