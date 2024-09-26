fcd71fc9a6e479ae904bff69cf0a48524eef86d4
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-26 18:19:00 +0100

    Add more specific checks

--- tests/testthat/test-mod_select_bundle.R
+++ tests/testthat/test-mod_select_bundle.R
@@ -1,4 +1,4 @@
-test_that("dropdown list server works", {
+test_that("select_bundle reacts to bundle selection", {
   testServer(mod_select_bundle_server, {
     ns <- session$ns
     # Pre-defined golem tests
@@ -7,8 +7,17 @@ test_that("dropdown list server works", {
     expect_true(grepl("test", ns("test")))
 
     out <- session$getReturned()
+    select_bundle <- "bundle_measurement"
+    session$setInputs(select_bundle = select_bundle)
+
     expect_true(is.reactive(out))
-    # FIXME: add more relevant checks
+    expect_s3_class(out(), "data.frame")
+    expect_true(all(c("concept_id", "concept_name") %in% names(out())),
+      info = "Return data should contain the columns 'concept_id' and 'concept_name'"
+    )
+
+    expected_concepts <- get_bundle_concepts_table("bundle_measurement", "measurement")$concept_id
+    expect_equal(out()$concept_id, expected_concepts)
   })
 })
 
@@ -20,4 +29,18 @@ test_that("module ui works", {
   for (i in c("id")) {
     expect_true(i %in% names(fmls))
   }
+})
+
+test_that("select_bundle server can return all concepts", {
+  testServer(mod_select_bundle_server, {
+    out <- session$getReturned()
+    session$setInputs(select_bundle = "all")
+
+    expect_true(is.reactive(out))
+    expect_s3_class(out(), "data.frame")
+    expect_true(all(c("concept_id", "concept_name") %in% names(out())),
+      info = "Return data should contain the columns 'concept_id' and 'concept_name'"
+    )
+    expect_equal(out()$concept_id, get_concepts_table()$concept_id)
+  })
 })
