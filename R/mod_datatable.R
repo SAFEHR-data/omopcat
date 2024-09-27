e9283d3482c591a094bc61af7ce99f1e5c05d41b
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-27 12:08:44 +0100

    Use a regular helper function instead of a second server function
    Splitting business-logic from application logic

--- R/mod_datatable.R
+++ R/mod_datatable.R
@@ -14,6 +14,7 @@ mod_datatable_ui <- function(id) {
   )
 }
 
+# FIXME: update docs
 #' datatable Server Functions
 #'
 #' @param data A reactive data.frame containing the data to be displayed
@@ -28,30 +29,28 @@ mod_datatable_server <- function(id, concepts, monthly_counts, selected_dates = 
   stopifnot(is.reactive(selected_dates) || is.null(selected_dates))
 
   moduleServer(id, function(input, output, session) {
-    dpc <- datatable_plus_counts_server("sdbc", concepts, monthly_counts, selected_dates)
-    output$datatable <- DT::renderDT(dpc(), selection = list(
+    concepts_with_counts <- reactive({
+      join_counts_to_concepts(concepts(), monthly_counts, selected_dates())
+    })
+    output$datatable <- DT::renderDT(concepts_with_counts(), selection = list(
       mode = "single",
       selected = 1,
       target = "row"
     ))
 
-    reactive(dpc()[input$datatable_rows_selected, ])
+    reactive(concepts_with_counts()[input$datatable_rows_selected, ])
   })
 }
 
-# use selected dates to calc num patients and records per concept
-# join onto selected_data
-datatable_plus_counts_server <- function(id, concepts, monthly_counts, selected_dates) {
-  moduleServer(id, function(input, output, session) {
-    reactive({
-      monthly_counts |>
-        filter_dates(selected_dates()) |>
-        dplyr::group_by(.data$concept_id) |>
-        dplyr::summarise(
-          records = sum(.data$record_count),
-          patients = round(sum(.data$record_count) / mean(.data$records_per_person))
-        ) |>
-        dplyr::right_join(concepts(), by = "concept_id")
-    })
-  })
+## Use selected dates to calculate number of patients and records per concept
+## and join onto selected_data
+join_counts_to_concepts <- function(concepts, monthly_counts, selected_dates) {
+  monthly_counts |>
+    filter_dates(selected_dates) |>
+    dplyr::group_by(.data$concept_id) |>
+    dplyr::summarise(
+      records = sum(.data$record_count),
+      patients = round(sum(.data$record_count) / mean(.data$records_per_person))
+    ) |>
+    dplyr::right_join(concepts, by = "concept_id")
 }
