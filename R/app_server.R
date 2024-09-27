76aea64cc281695c0c9421d5ca64a61700d65761
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-27 12:15:07 +0100

    Comment main server functions

--- R/app_server.R
+++ R/app_server.R
@@ -9,15 +9,21 @@ app_server <- function(input, output, session) {
   concepts_table <- get_concepts_table()
   monthly_counts <- get_monthly_counts()
 
+  # Filter concepts table based on user-selected concepts, showing all by default
   selected_data <- mod_select_concepts_server("select_concepts", concepts_table)
 
+  # Get the selected dates for date-range filtering
   selected_dates <- mod_date_range_server("date_range")
 
-  # populate main concepts table
+  # Populate the main concepts table in the dashboard with selected concepts and their
+  # records and patients counts
+  # Get the selected row from the datatable as a reactive output
   selected_row <- mod_datatable_server("totals", selected_data, monthly_counts, selected_dates)
 
+  # Generate the plots based on the selected data
   mod_plots_server("monthly_counts", selected_row, selected_dates, type = "monthly_counts")
   mod_plots_server("summary_stats", selected_row, type = "summary_stats")
 
+  # Generate the export tab based on the selected data
   mod_export_tab_server("export_tab", selected_data)
 }
