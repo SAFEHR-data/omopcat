4ea2b9254645c4c8dba0066d7a1088346d2effde
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-03 13:34:42 +0100

    Set default ggplot2 theme with larger font size

--- R/mod_plots.R
+++ R/mod_plots.R
@@ -33,6 +33,14 @@ mod_plots_server <- function(id, selected_concept, selected_dates) {
   stopifnot(is.reactive(selected_concept))
   stopifnot(is.reactive(selected_dates))
 
+  ## Set default theme for ggplot2
+  ggplot2::theme_set(
+    ggplot2::theme_minimal(
+      base_size = 16,
+      base_family = "sans"
+    )
+  )
+
   moduleServer(id, function(input, output, session) {
     selected_concept_id <- reactive(selected_concept()$concept_id)
     selected_concept_name <- reactive(selected_concept()$concept_name)
