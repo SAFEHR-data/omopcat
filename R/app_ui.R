8250450868e693a7ef0a463f3daa92396939c715
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-03 16:30:11 +0100

    Move date-range selection to top of sidebar

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,9 +14,9 @@ app_ui <- function(request) {
       title = .app_title(),
       sidebar = sidebar(
         title = "Filtering options",
-        mod_select_bundle_ui("select_bundle"),
-        mod_select_concepts_ui("select_concepts"),
         mod_date_range_ui("date_range"),
+        mod_select_bundle_ui("select_bundle"),
+        mod_select_concepts_ui("select_concepts")
       ),
       nav_panel(
         title = "Concepts",
