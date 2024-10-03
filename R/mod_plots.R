19b17078d7f21207ada8ddc2b262f37d4d05f69b
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-03 14:36:59 +0100

    Put card title and tabs at the same hight for summary stat plots

--- R/mod_plots.R
+++ R/mod_plots.R
@@ -16,12 +16,10 @@ mod_plots_ui <- function(id) {
         card_header("Distribution of Monthly Records for the selected concepts"),
         plotly::plotlyOutput(ns("monthly_counts"))
       ),
-      card(
-        card_header("Summary Statistics for the selected concepts"),
-        navset_card_underline(
-          nav_panel("Numeric concepts", plotOutput(ns("numeric_stats"))),
-          nav_panel("Categorical concepts", plotOutput(ns("categorical_stats"))),
-        )
+      navset_card_underline(
+        title = "Summary Statistics for the selected concepts",
+        nav_panel("Numeric concepts", plotOutput(ns("numeric_stats"))),
+        nav_panel("Categorical concepts", plotOutput(ns("categorical_stats"))),
       )
     )
   )
