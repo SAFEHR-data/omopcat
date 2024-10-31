01dae5c25a31f8e6e428ab1bbead772184a06a6d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-31 17:34:58 +0000

    Add basic tests

---
+++ preprocessing/tests/testthat/test-preprocessing.R
@@ -0,0 +1,29 @@
+# Disable cli messages
+withr::local_options(usethis.quiet = TRUE, cli.default_handler = function(...) {})
+
+test_that("preprocessing fails without valid out_path", {
+  expect_error(preprocess(out_path = ""), "should not be empty")
+})
+
+test_that("preprocessing is skipped if files already exist", {
+  out_path <- tempdir()
+  out_files <- c(
+    file.path(out_path, "omopcat_concepts.parquet"),
+    file.path(out_path, "omopcat_monthly_counts.parquet"),
+    file.path(out_path, "omopcat_summary_stats.parquet")
+  )
+  fs::file_create(out_files)
+  expect_false(preprocess(out_path = out_path))
+})
+
+test_that("preprocessing fails in production if envvars are missing", {
+  withr::local_envvar(
+    ENV = "prod",
+    DB_NAME = NULL,
+    HOST = NULL,
+    PORT = NULL,
+    DB_USERNAME = NULL,
+    DB_PASSWORD = NULL
+  )
+  expect_error(preprocess(out_path = "foo"), "not set")
+})
