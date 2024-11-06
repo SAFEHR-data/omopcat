e8b4ef0240c424177262f96f7ae2370911e02313
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-06 15:53:19 +0000

    release: Prepare for `0.2.1` release (#106)

--- R/utils_get_data.R
+++ R/utils_get_data.R
@@ -27,7 +27,8 @@ get_concepts_table <- function() {
   # Make sure the concept IDs are integers so that they get rendered as such
   # in shiny::renderTable()
   ct$concept_id <- as.integer(ct$concept_id)
-  ct
+  # Remove "no matching concept" entries
+  dplyr::filter(ct, .data$concept_id != 0)
 }
 
 get_monthly_counts <- function() {
