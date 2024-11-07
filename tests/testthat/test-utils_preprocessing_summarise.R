c343813029d21fbdc4d787f4d1cc3206f79a8335
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-07 11:23:53 +0000

    Move over monthly count tests to preprocessing package

--- tests/testthat/test-utils_preprocessing_summarise.R
+++ tests/testthat/test-utils_preprocessing_summarise.R
@@ -1,32 +1,3 @@
-## Set up a mock measurement OMOP table
-## Measurements for 3 different patients on the same day, with 1 patient having 2 measurements
-measurement <- data.frame(
-  measurement_id = 1:4,
-  person_id = c(1, 1, 2, 3),
-  measurement_type_concept_id = 12345,
-  measurement_concept_id = 1,
-  measurement_date = as.Date("2020-01-01"),
-  value_as_number = c(2, 1, 2, 1),
-  value_as_concept_id = 0
-)
-
-test_that("calculate_monthly_counts produces the expected results", {
-  res <- calculate_monthly_counts(measurement, measurement_concept_id, measurement_date)
-  expect_s3_class(res, "data.frame")
-  expect_named(res, c("concept_id", "date_year", "date_month", "record_count", "person_count", "records_per_person"))
-  expect_equal(nrow(res), 1)
-  expect_equal(res$person_count, 3)
-  expect_equal(res$records_per_person, 4 / 3)
-})
-
-db <- dbplyr::src_memdb()
-db_measurement <- dplyr::copy_to(db, measurement, name = "measurement", overwrite = TRUE)
-test_that("calculate_monthly_counts works on Database-stored tables", {
-  res <- calculate_monthly_counts(db_measurement, measurement_concept_id, measurement_date)
-  expect_s3_class(res, "data.frame")
-  expect_named(res, c("concept_id", "date_year", "date_month", "record_count", "person_count", "records_per_person"))
-})
-
 test_that("calculate_summary_stats produces the expected results", {
   res <- calculate_summary_stats(measurement, "measurement_concept_id")
   expect_s3_class(res, "data.frame")
