76c1e942f67b641a83becfa79f430c4435370482
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-07 17:58:03 +0000

    Move test DB creating script

--- data-raw/test_db/create_test_db.R
+++ data-raw/test_db/create_test_db.R
@@ -1,22 +1,28 @@
+# Creates a test database from the synthea-allergies-10k example dataset
+# with added dummy data for observations and measurements
+
 # PRODUCED FOR A SPECIFIC DATASET:
 # synthea-allergies-10k
 # (but could work for others)
-
-cli::cli_h1("Inserting dummy tables")
 
 
 # Setup ---------------------------------------------------------------------------------------
 
 library(readr)
-library(omopcat)
 
-# TODO: add here::i_am()
+here::i_am("data-raw/test_db/create_test_db.R")
 
 dir <- here::here("data-raw/test_db/eunomia")
 name <- "synthea-allergies-10k"
 version <- "5.3"
 
-db_path <- glue::glue("{dir}/{name}_{version}_1.0.duckdb")
+Sys.setenv(EUNOMIA_DATA_FOLDER = dir)
+
+db_path <- CDMConnector::eunomia_dir(
+  dataset_name = name,
+  cdm_version = version,
+  database_file = glue::glue("{dir}/{name}_{version}_1.0.duckdb")
+)
 
 con <- DBI::dbConnect(duckdb::duckdb(db_path))
 withr::defer(DBI::dbDisconnect(con))
@@ -73,19 +79,4 @@ dummy_observations <- read_csv(
 )
 write_table(dummy_observations, con, "observation", schema = "main")
 
-# Sanity check: read the data back and make sure its consistent
-db_measurements <- DBI::dbReadTable(con, "measurement")
-stopifnot(all.equal(db_measurements, as.data.frame(dummy_measurements)))
-
-db_observations <- DBI::dbReadTable(con, "observation")
-stopifnot(all.equal(db_observations, as.data.frame(dummy_observations)))
-
-# Load the CMD object to verify integrity of the schema after insertions
-cdm <- CDMConnector::cdm_from_con(
-  con = con,
-  cdm_schema = "main",
-  write_schema = "main",
-  cdm_name = name
-)
-
-cli::cli_alert_success("Dummy tables inserted successfully")
+cli::cli_alert_success("Test database set up successfully")
