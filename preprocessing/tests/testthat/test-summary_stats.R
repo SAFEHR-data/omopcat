a9b1fbdd800efacbbd520e3d4573356404727b29
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-07 14:48:13 +0000

    Handle numeric and categorical concepts together in summary_stats tests

--- preprocessing/tests/testthat/test-summary_stats.R
+++ preprocessing/tests/testthat/test-summary_stats.R
@@ -2,34 +2,32 @@ test_that("generate_summary_stats works on a CDM object", {
   cdm <- .setup_cdm_object()
   summary_stats <- generate_summary_stats(cdm)
   expect_s3_class(summary_stats, "data.frame")
-  expect_true(nrow(summary_stats) > 0)
-  expect_named(summary_stats, c())
+  expect_named(summary_stats, c(
+    "concept_id", "concept_name", "summary_attribute", "value_as_number", "value_as_string"
+  ))
 })
 
+## Mock measurments for 2 concepts
+mock_measurements <- data.frame(
+  measurement_id = 1:8,
+  person_id = 1,
+  measurement_type_concept_id = rep(c(12345, 23456), each = 4),
+  measurement_concept_id = rep(c(1, 2), each = 4),
+  measurement_date = "2020-01-01",
+  value_as_number = c(2, 1, 2, 1, rep(NA, 4)),
+  value_as_concept_id = c(rep(0, 4), c(1, 1, 2, 3))
+)
+
 test_that("calculate_summary_stats produces the expected results", {
-  res <- calculate_summary_stats(measurement, "measurement_concept_id")
+  res <- calculate_summary_stats(mock_measurements, "measurement_concept_id")
   expect_s3_class(res, "data.frame")
   expect_named(res, c("concept_id", "summary_attribute", "value_as_number", "value_as_concept_id"))
-  expect_equal(nrow(res), 2)
+  expect_equal(nrow(res), 5)
   mean <- res[res$summary_attribute == "mean", ][["value_as_number"]]
   sd <- res[res$summary_attribute == "sd", ][["value_as_number"]]
   expect_equal(mean, 1.5)
   expect_equal(sd^2, 1 / 3)
-})
 
-## Add a categorical concept
-categorical_measurement <- data.frame(
-  measurement_id = 1:4,
-  person_id = c(1, 1, 2, 3),
-  measurement_type_concept_id = 12345,
-  measurement_concept_id = 2,
-  measurement_date = as.Date("2020-01-01"),
-  value_as_number = NA,
-  value_as_concept_id = c(1, 1, 2, 3)
-)
-measurement <- rbind(measurement, categorical_measurement)
-test_that("calculate_summary_stats can handle categorical concepts", {
-  res <- calculate_summary_stats(measurement, "measurement_concept_id")
   frequencies <- res[res$summary_attribute == "frequency", ]
   expect_equal(nrow(frequencies), 3)
   expect_equal(frequencies$value_as_number, c(2, 1, 1))
