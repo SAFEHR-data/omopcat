ce8bc416f65019a2e217e198cf42c523736f8b5a
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-08 11:22:50 +0000

    Fix `create_test_db` script

--- data-raw/test_db/create_test_db.R
+++ data-raw/test_db/create_test_db.R
@@ -15,16 +15,17 @@ here::i_am("data-raw/test_db/create_test_db.R")
 dir <- here::here("data-raw/test_db/eunomia")
 name <- "synthea-allergies-10k"
 version <- "5.3"
+schema <- "main"
 
-Sys.setenv(EUNOMIA_DATA_FOLDER = dir)
-
-db_path <- CDMConnector::eunomia_dir(
-  dataset_name = name,
-  cdm_version = version,
-  database_file = glue::glue("{dir}/{name}_{version}_1.0.duckdb")
+# Download the example data if it doesn't exist yet and create the duckdb database
+withr::with_envvar(
+  new = c(EUNOMIA_DATA_FOLDER = dir),
+  {
+    invisible(CDMConnector::eunomia_dir(dataset_name = name, cdm_version = version))
+  }
 )
 
-con <- DBI::dbConnect(duckdb::duckdb(db_path))
+con <- DBI::dbConnect(duckdb::duckdb(glue::glue("{dir}/{name}_{version}_1.1.duckdb")))
 withr::defer(DBI::dbDisconnect(con))
 
 
@@ -59,24 +60,54 @@ dummy_measurements <- read_csv(
     measurement_date = col_date(),
     value_as_number = col_double(),
     value_as_concept_id = col_integer(),
+    measurement_time = col_character(),
+    measurement_type_concept_id = col_integer(),
+    operator_concept_id = col_integer(),
+    unit_concept_id = col_integer(),
+    range_low = col_number(),
+    range_high = col_number(),
+    provider_id = col_integer(),
+    visit_occurrence_id = col_integer(),
+    visit_detail_id = col_integer(),
+    measurement_source_value = col_character(),
+    measurement_source_concept_id = col_integer(),
+    unit_source_value = col_character(),
+    value_source_value = col_character()
   )
 )
-write_table(dummy_measurements, con, "measurement", schema = "main")
+write_table(dummy_measurements, con, "measurement", schema = schema)
 
 dummy_observations <- read_csv(
-  here::here(
-    "data-raw/test_db/dummy/observation.csv"
-  ),
+  here::here("data-raw/test_db/dummy/observation.csv"),
   col_types = cols(
     observation_id = col_integer(),
     person_id = col_integer(),
     observation_concept_id = col_integer(),
     observation_date = col_date(),
     value_as_number = col_double(),
-    value_as_string = col_logical(),
+    value_as_string = col_character(),
     value_as_concept_id = col_integer(),
+    observation_type_concept_id = col_integer(),
+    qualifier_concept_id = col_integer(),
+    unit_concept_id = col_integer(),
+    provider_id = col_integer(),
+    visit_occurrence_id = col_integer(),
+    visit_detail_id = col_integer(),
+    observation_source_value = col_character(),
+    observation_source_concept_id = col_integer(),
+    unit_source_value = col_character(),
+    qualifier_source_value = col_character()
   )
 )
-write_table(dummy_observations, con, "observation", schema = "main")
+write_table(dummy_observations, con, "observation", schema = schema)
+
+## Verify integrity, turn warnings into error
+tryCatch(
+  cdm <- CDMConnector::cdm_from_con(con, cdm_schema = schema, write_schema = schema),
+  warning = function(cnd) {
+    msg <- sprintf("CDM integrity check produced warnings: %s", conditionMessage(cnd))
+    rlang::abort(msg, call = NULL)
+  }
+)
 
 cli::cli_alert_success("Test database set up successfully")
