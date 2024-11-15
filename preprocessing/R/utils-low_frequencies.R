9a9f98011fe6334c9c37edcf889c0070e90b3258
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-15 10:47:44 +0100

    Re-implement `replace_low-frequencies()` in preprocessing pkg

---
+++ preprocessing/R/utils-low_frequencies.R
@@ -0,0 +1,40 @@
+#' Helper function to replace low frequencies
+#'
+#' Replaces frequencies below a certain threshold with a new value, based on the
+#' `LOW_FREQUENCY_THRESHOLD` and `LOW_FREQUENCY_REPLACEMENT` environment variables.
+#' Values equal to zero are filtered out prior to replacement.
+#'
+#' @param df A data.frame
+#' @param cols A character vector of column names for which to apply the replacement
+#' @param threshold Threshold value below which values will be replaced by `replacement`
+#' @param replacement Value with which values below `threshold` will be replaced
+#'
+#' @return A numeric vector with low frequencies replaced
+#' @keywords internal
+replace_low_frequencies <- function(df, cols, threshold, replacement) {
+  threshold_num <- .as_numeric(threshold, "threshold")
+  replacement_num <- .as_numeric(replacement, "replacement")
+
+  # Remove records with values equal to 0
+  df <- dplyr::filter(df, dplyr::if_all(dplyr::all_of(cols), ~ . > 0))
+
+  # Replace values below the threshold with the replacement value
+  dplyr::mutate(
+    df, dplyr::across(
+      dplyr::all_of(cols),
+      ~ ifelse(.x < threshold_num, replacement_num, .x)
+    )
+  )
+}
+
+# Turn "NAs introduced by coercion" warnings into error
+.as_numeric <- function(x, arg, call = rlang::caller_env()) {
+  tryCatch(as.numeric(x),
+    warning = function(cnd) {
+      cli::cli_abort(c(
+        "{.arg {arg}} is not a valid number.",
+        "x" = "Got {.var {arg}} = {x}"
+      ), call = call)
+    }
+  )
+}
