1cabeb71525a07b9179911382c933f0a980064fd
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-18 17:33:55 +0100

    feat(preprocessing): remove `no matching concept` instances
    Closes #91

--- preprocessing/R/concepts.R
+++ preprocessing/R/concepts.R
@@ -9,6 +9,7 @@ generate_concepts <- function(cdm, concept_ids) {
   # Extract columns from concept table
   cdm$concept |>
     dplyr::filter(.data$concept_id %in% concept_ids) |>
+    dplyr::filter(.data$concept_id != 0) |> # remove "no matching concept" instances
     dplyr::select(
       "concept_id",
       "concept_name",
--- preprocessing/tests/testthat/test-concepts.R
+++ preprocessing/tests/testthat/test-concepts.R
@@ -8,3 +8,10 @@ test_that("generate_concepts works on a CDM object", {
   expect_type(concepts$concept_id, "integer")
   expect_type(concepts$concept_name, "character")
 })
+
+test_that("'No matching concept' concepts are removed", {
+  # Explicitly request concept_id 0 "No matching concept"
+  concepts <- generate_concepts(mock_cdm, concept_ids = c(0, 1569708, 3020630))
+  expect_false(0 %in% concepts$concept_id)
+  expect_equal(nrow(concepts), 2)
+})
