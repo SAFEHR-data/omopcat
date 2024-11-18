a04729dbe6f5664924634090ed606e299646f4e3
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-18 17:11:32 +0100

    test(preprocessing): test a range of replacement values

--- preprocessing/tests/testthat/test-utils-low_frequencies.R
+++ preprocessing/tests/testthat/test-utils-low_frequencies.R
@@ -2,15 +2,16 @@ mock_data <- data.frame(x = c(0, 1, 2, 3), y = c(9, 8, 1, 0), z = letters[1:4])
 
 test_that("replace_low_frequencies works", {
   cols <- c("x", "y")
-  replacement <- 0.5
 
-  ## Test a range of thresholds
+  ## Test a range of thresholds and replacement values
   for (threshold in seq(-10, 10)) {
-    out <- replace_low_frequencies(mock_data, cols,
-      threshold = threshold, replacement = replacement
-    )
-    expect_true(all(out[cols] > 0))
-    expect_true(all(out[cols] >= threshold | out[cols] == replacement))
+    for (replacement in seq(1, 5)) {
+      out <- replace_low_frequencies(mock_data, cols,
+        threshold = threshold, replacement = replacement
+      )
+      expect_true(all(out[cols] > 0))
+      expect_true(all(out[cols] >= threshold | out[cols] == replacement))
+    }
   }
 })
 
