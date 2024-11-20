8ec2195b435a2717e6376731415a959dc5547058
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-11-20 18:34:02 +0100

    Handle quarterly counts in the app

--- app/R/fct_monthly_count_plot.R
+++ app/R/fct_monthly_count_plot.R
@@ -16,7 +16,10 @@ monthly_count_plot <- function(monthly_counts) {
   stopifnot(is.data.frame(monthly_counts))
   stopifnot(all(c("date_year", "date_month", "person_count") %in% colnames(monthly_counts)))
 
-  monthly_counts$date <- .convert_to_date(monthly_counts$date_year, monthly_counts$date_month)
+  monthly_counts$date <- lubridate::make_date(
+    year = monthly_counts$date_year,
+    month = monthly_counts$date_month
+  )
 
   ggplot(monthly_counts, aes(x = .data$date, y = .data$record_count)) +
     geom_bar(aes(fill = .data$concept_name), stat = "identity") +
@@ -25,8 +28,4 @@ monthly_count_plot <- function(monthly_counts) {
     labs(fill = NULL) +
     scale_x_date(date_labels = "%b %Y", date_breaks = "3 months") +
     theme(legend.position = "none", axis.text.x = element_text(angle = 45, hjust = 1))
-}
-
-.convert_to_date <- function(date_year, date_month) {
-  as.Date(paste0(date_year, "-", date_month, "-01"))
 }
