1fe2649bc5f4482e618bd441eea202f131ce264d
Stef Piatek <s.piatek@ucl.ac.uk>
2025-11-13 11:33:39 +0000

    Create nginx config at runtime

--- www/create_certificates_and_run_nginx.sh
+++ www/create_certificates_and_run_nginx.sh
@@ -2,13 +2,14 @@
 set -euxo pipefail
 
 if [ "$SERVER_NAME" != "localhost" ]; then
-    if [ ! -f /etc/letsencrypt/live/$SERVER_NAME/fullchain.pem ]; then
-        echo "Certificates not found, creating them"
+    if [ ! -f /etc/nginx/conf.d/default.conf ]; then
+        echo "Config file not found, creating it"
+        envsubst '${SERVER_NAME}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
         /usr/bin/certbot --nginx --non-interactive --agree-tos -d "$SERVER_NAME"
     fi
-    echo "Certificates found, running nginx"
+    echo "Config file found, running nginx"
     nginx -g 'daemon off;'
 fi
 
-echo "No certificates needed, running nginx"
+echo "Running locally, running nginx without config file"
 nginx -g 'daemon off;'
